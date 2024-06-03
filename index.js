c7fb480e217e1c565b959a820f246a8826bc3477
VxneC0de <vanerubio2020@gmail.com>
2024-06-03 15:04:30 -0400

    feat: added new file and new javascript FizzBuzz exercise#

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+-When a user inputs a number
+-Loop from 1 to the entered number
+-If the current number is divisible by 3 then print "Fizz"
+-If the current number is divisible by 5 then print "Buzz"
+-If the current number is divisible by 3 and 5 then print "FizzBuzz"
+-Otherwise print the current number
+*/
+
+let enter = parseInt(prompt("Enter a number"));
+
+
+for (let i = 1; i <= enter; i++) {
+
+  if (i % 3 && i % 5 === 0) {
+      console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+      console.log("Fizz");
+  } else if (i % 5 === 0) {
+      console.log("Buzz");
+  } else {
+      console.log(i);
+  }
+
+}
+
